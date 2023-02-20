28ee73957382bf19c538ded8dbd132971989136b
kagemeka <kagemeka1@gmail.com>
2023-02-20 10:33:12 +0900

    update clang-format config

--- dsalgo/dsalgo.hpp
+++ dsalgo/dsalgo.hpp
@@ -1,3 +1,2 @@
 #pragma once
-
 #include "union_find.hpp"
--- dsalgo/union_find.hpp
+++ dsalgo/union_find.hpp
@@ -1,7 +1,5 @@
 #pragma once
-
 #include <vector>
-
 namespace dsalgo::union_find {
 class union_find {};
 } // namespace dsalgo::union_find
